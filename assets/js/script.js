b1d715cbe8e3ef1c92d2791465c1c6f449933c71
fernkari (∩◕-◕ )⊃━✿✿✿✿ <fernkari@amazon.com>
2021-11-10 14:04:56 -0800

    (∩◕-◕ )⊃━✿✿✿✿ Use the form data to create the tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,15 @@ let tasksToDoEl = document.querySelector("#tasks-to-do");
 
 const createTaskHandler = function(event) {
     event.preventDefault();
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     let taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "This is a new task";
     taskItemEl.className = "task-item";
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskItemEl.appendChild(taskInfoEl);
     tasksToDoEl.appendChild(taskItemEl);
 }
 formEl.addEventListener("submit", createTaskHandler);
